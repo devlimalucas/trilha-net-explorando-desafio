5a5e07aa8262cfd55469fd762cda993a8df6f4de
Lucas Lima <94488633+devlimalucas@users.noreply.github.com>
2025-10-23 17:59:08 -0300

    feat: calcula o valor total da hospedadem e aplica o desconto caso necessário

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,5 @@
+using System.Xml.XPath;
+
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -48,13 +50,15 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                return valor * 0.9m;
             }
 
             return valor;
